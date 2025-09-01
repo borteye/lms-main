3cdc01ea8361ef3121acf9d5e282b0883d49174d
borteye <gabrielborteye12199@gmail.com>
2025-09-01 11:53:48 +0000

    feat(auth): add signup schema and type

---
+++ packages/common/src/lib/auth.ts
@@ -0,0 +1,22 @@
+import z from "zod";
+
+export const signUpSchema = z
+  .object({
+    firstName: z.string().min(1, "First name is required"),
+    lastName: z.string().min(1, "Last name is required"),
+    email: z.string().email("Please enter a valid email address"),
+    phoneNumber: z
+      .string()
+      .min(1, "Phone number is required")
+      .min(10, "Please enter a valid phone number"),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters" }),
+    passwordConfirmation: z.string(),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    path: ["passwordConfirmation"],
+    message: "Passwords do not match",
+  });
+
+export type SignUpData = z.infer<typeof signUpSchema>;
